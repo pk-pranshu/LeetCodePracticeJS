45423c32b2bf16d2be38ac11982fc7fbe7efec5c
PranshuKumar <pranu92@gmail.com>
2016-07-23 23:15:28 -0700

    Solved some more

--- Climbing Stairs.js
+++ Climbing Stairs.js
@@ -3,3 +3,37 @@
 // You are climbing a stair case. It takes n steps to reach to the top.
 
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var len = s.length,
+        map = {},
+        result = 0,
+        i;
+    
+    if (len === 0) {
+        return 0;
+    }
+    
+    map['I'] = 1;
+    map['V'] = 5;
+    map['X'] = 10;
+    map['L'] = 50;
+    map['C'] = 100;
+    map['D'] = 500;
+    map['M'] = 1000;
+    
+    for (i = 0; i < len; i++) {
+        result += map[s.charAt(i)];
+        
+        if (i > 0 && map[s.charAt(i)] > map[s.charAt(i - 1)]) {
+            result -= 2 * map[s.charAt(i - 1)];
+        }
+    }
+    
+    return result;
+};
--- Roman to Integer.js
+++ Roman to Integer.js
@@ -3,3 +3,36 @@
 // Given a roman numeral, convert it to an integer.
 
 // Input is guaranteed to be within the range from 1 to 3999.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var len = s.length,
+        map = {},
+        result = 0,
+        i;
+    
+    if (len === 0) {
+        return 0;
+    }
+    
+    map['I'] = 1;
+    map['V'] = 5;
+    map['X'] = 10;
+    map['L'] = 50;
+    map['C'] = 100;
+    map['D'] = 500;
+    map['M'] = 1000;
+    
+    for (i = 0; i < len; i++) {
+        result += map[s.charAt(i)];
+        
+        if (i > 0 && map[s.charAt(i)] > map[s.charAt(i - 1)]) {
+            result -= 2 * map[s.charAt(i - 1)];
+        }
+    }
+    
+    return result;
+};
