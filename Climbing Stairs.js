5bd54b59c7ae5816bafda996c08b6a15a4903689
PranshuKumar <pranu92@gmail.com>
2016-07-23 23:16:22 -0700

    Solved some more

--- Climbing Stairs.js
+++ Climbing Stairs.js
@@ -6,34 +6,37 @@
 
 
 /**
- * @param {string} s
+ * @param {number} n
  * @return {number}
  */
-var romanToInt = function(s) {
-    var len = s.length,
-        map = {},
-        result = 0,
-        i;
-    
-    if (len === 0) {
-        return 0;
+ var climbStairs = function(n) {
+    var result = 0;
+    var stepOne = 1;
+    var stepTwo = 2;
+    if (n === 1) return stepOne;
+    if (n === 2) return stepTwo;
+
+    for (var i = 3; i <= n; i++) {
+        result = stepOne + stepTwo;
+        stepOne = stepTwo;
+        stepTwo = result;
     }
-    
-    map['I'] = 1;
-    map['V'] = 5;
-    map['X'] = 10;
-    map['L'] = 50;
-    map['C'] = 100;
-    map['D'] = 500;
-    map['M'] = 1000;
-    
-    for (i = 0; i < len; i++) {
-        result += map[s.charAt(i)];
-        
-        if (i > 0 && map[s.charAt(i)] > map[s.charAt(i - 1)]) {
-            result -= 2 * map[s.charAt(i - 1)];
-        }
-    }
-    
+
     return result;
+
 };
+
+
+// var climbStairs = function(n) {
+//     var arr = [],
+//         i;
+    
+//     arr[0] = 0;
+//     arr[1] = 1;
+//     arr[2] = 2;
+    
+//     for(i = 3; i <= n; i++) {
+//         arr[i] = arr[i - 1] + arr[i - 2];
+//     }
+//     return arr[n];
+// };
