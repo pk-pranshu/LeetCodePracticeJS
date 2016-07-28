6b6080ec76bd5e011805a116a88e7b8a396bb65b
PranshuKumar <pranu92@gmail.com>
2016-07-27 21:15:53 -0700

    Added more

--- Palindrome Number.js
+++ Palindrome Number.js
@@ -10,3 +10,26 @@
 var isPalindrome = function(x) {
     return x == x.toString().split('').reverse().join('');
 };
+
+
+
+
+
+Method 2 :-
+
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var a=0;
+    var temp=x;
+    if(x<0) return false;
+    while(temp>0){
+        a=a*10+temp%10;
+        temp=Math.floor(temp/10);
+    }
+    if(x===a) return true;
+    else return false;
+};
